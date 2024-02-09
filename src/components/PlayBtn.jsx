ea964a6e681875119efcdb4f394d80954e5e89cf
mrrmartin01 <Theophilusmartin123@gmail.com>
2024-02-09 14:29:59 +0000

    final header mapping

--- src/components/PlayBtn.jsx
+++ src/components/PlayBtn.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import './playBtn.css';
 
-function PlayBtn() {
+function PlayBtn({movie}) {
   return (
-    <div className="trailer d-flex align-items-center justify-content-center">
-        <a href="#" className="playBtn">
+    <div className={`trailer d-flex align-items-center justify-content-center ${ movie.active ? "active" : undefined}`}>
+        <a href="#/" className="playBtn">
             <ion-icon name="play-outline"></ion-icon>
         </a>
         <p>watch anime here</p>
@@ -12,4 +12,4 @@ function PlayBtn() {
   )
 }
 
-export default PlayBtn
+export default PlayBtn;
--- src/pages/Banner.jsx
+++ src/pages/Banner.jsx
@@ -1,54 +1,64 @@
-import React, {useState, useEffect} from 'react';
-import './banner.css';
-import bgImg from '../images/Attack_on_Titans_preview.jpg'
-import MovieContent from '../components/MovieContent';
-import MovieDate from '../components/MovieDate';
-import PlayBtn from '../components/PlayBtn';
-import MovieSwiper from '../components/MovieSwiper';
+import React, { useState, useEffect } from "react";
+import "./banner.css";
+import MovieContent from "../components/MovieContent";
+import MovieDate from "../components/MovieDate";
+import PlayBtn from "../components/PlayBtn";
+import MovieSwiper from "../components/MovieSwiper";
 
 function Banner() {
-    const [movies, setMovies] = useState([]);
+  const [movies, setMovies] = useState([]);
 
-    const fetchData = () => {
-        fetch('http://localhost:3000/data/movieData.json')
-        .then(response => response.json())
-        .then(data => setMovies(data))
-        .catch(e=> console.log(e.message));
-    };
+  const fetchData = () => {
+    fetch("http://localhost:3000/data/movieData.json")
+      .then((response) => response.json())
+      .then((data) => setMovies(data))
+      .catch((e) => console.log(e.message));
+  };
 
-    useEffect(() => {
-        fetchData();
-    }, []);
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-    const handleSlideChange = id =>{
-        console.log(id)
-    }
-    
+  const handleSlideChange = (id) => {
+    const newMovies = movies.map(movie => {
+        movie.active = false;
+        if  (movie._id === id) {
+            movie.active = true;
+        }
+       return movie;
+    });
+    setMovies(newMovies);
+  };
+
   return (
-    <div className='banner'>
-        {
-            movies && movies.length>0 && movies.map(movie => (
-                <div className="movie" key={movie.id}>
-                    <img src={movies.bgImg} alt="Background Img" className='bgImg'></img>
-                    <div className="container-fluid">
-                        <div className="row">
-                            <div className='col-lg-6 col-md-12'>
-                                <MovieContent/>
-                            </div>
-                            <div className='col-lg-6 col-md-12'>
-                                <MovieDate/>
-                                <PlayBtn/>
-                            </div>
-                        </div>
-                    </div>
+    <div className="banner">
+      {movies &&
+        movies.length > 0 &&
+        movies.map((movie) => (
+          <div className="movie">
+            <img
+              src={movie.previewImg}
+              alt="Background Img"
+              className={`bgImg ${movie.active ? "active" : undefined}`}
+            ></img>
+            <div className="container-fluid">
+              <div className="row">
+                <div className="col-lg-6 col-md-12">
+                  <MovieContent  movie ={movie}/>
                 </div>
-            ))
-        }
-        
-        {
-            movies && movies.length>0 && <MovieSwiper slides={movies} slideChange={handleSlideChange} />
-        }
+                <div className="col-lg-6 col-md-12">
+                  <MovieDate movie ={movie}/>
+                  <PlayBtn movie={movie} />
+                </div>
+              </div>
+            </div>
+          </div>
+        ))}
+
+      {movies && movies.length > 0 && (
+        <MovieSwiper slides={movies} slideChange={handleSlideChange} />
+      )}
     </div>
-  )
+  );
 }
 export default Banner;
