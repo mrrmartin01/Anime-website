cb229fc03f2f248afa314883fe175c993563bdf4
mrrmartin01 <Theophilusmartin123@gmail.com>
2024-02-28 23:00:46 +0000

    header.jsx

--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -1,29 +1,29 @@
-import React from 'react';
-import './header.css'
-import NavListItems from '../components/NavListItems';
-import navListData from '../data/navListData';
-import Search from '../components/Search';
-import Button from '../components/Button';
+import React from "react";
+import "./header.css";
+import NavListItems from "../components/NavListItems";
+import navListData from "../data/navListData";
+import Search from "../components/Search";
+import Button from "../components/Button";
 
 function Header() {
   return (
     <header>
-        <a href="./" className="logo"> AnimeHub</a>
-        <ul className="nav">
-            {
-                navListData.map(nav => (
-                    <NavListItems key ={nav._id} nav ={ nav}/>
-
-                ))
-            }
-
-        </ul>
-        <Search/>
-        <Button icon={<ion-icon name="Log-in-outline"></ion-icon>}
-        name='Sign in'
-        />
+      <a href="./" className="logo">
+        {" "}
+        AnimeHub
+      </a>
+      <ul className="nav">
+        {navListData.map((nav) => (
+          <NavListItems key={nav._id} nav={nav} />
+        ))}
+      </ul>
+      <Search />
+      <Button
+        icon={<ion-icon name="Log-in-outline"></ion-icon>}
+        name="Sign in"
+      />
     </header>
-  )
+  );
 }
 
-export default Header
+export default Header;
